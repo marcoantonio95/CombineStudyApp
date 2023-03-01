13c5181fa006966a49071bd9339dda09b1175700
Marco Antonio Oliveira <marco.antonio9858@gmail.com>
2023-03-01 10:34:16 -0300

    Initial Commit

---
+++ CombineStudyApp/CombineStudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineStudyAppApp.swift
+//  CombineStudyApp
+//
+//  Created by Marco Antonio Oliveira on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineStudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineStudyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineStudyApp
+//
+//  Created by Marco Antonio Oliveira on 01/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
