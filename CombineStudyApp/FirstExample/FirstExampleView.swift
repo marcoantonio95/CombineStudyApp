d78d6d3befd060490a7c80ae913e480b611d3c7d
Marco Antonio Oliveira <marco.antonio9858@gmail.com>
2023-03-02 12:33:32 -0300

    feat: Update Assets
    Update and add color to assets

---
+++ CombineStudyApp/FirstExample/FirstExampleView.swift
@@ -0,0 +1,63 @@
+//
+//  FirstExampleView.swift
+//  CombineStudyApp
+//
+//  Created by Marco Antonio Oliveira on 01/03/23.
+//
+
+import Combine
+import Foundation
+import SwiftUI
+
+final class FirstExampleViewModel: ObservableObject {
+    @Published var savedText: String = ""
+}
+
+struct FirstExampleView: View {
+    @State var text: String = ""
+
+    @ObservedObject private var viewModel = FirstExampleViewModel()
+
+    var body: some View {
+        VStack {
+            VStack {
+                RoundedRectangle(
+                    cornerSize: CGSize.init(width: 40, height: 40))
+                    .fill(Color("neutralColor"))
+                    .frame(height: 180.0, alignment: .center)
+                    .overlay(
+                        VStack {
+                            Text(viewModel.savedText)
+                            VStack(spacing: 16) {
+                            TextField("Type something..", text: $text).padding(12)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 16)
+                                        .stroke(Color("primaryColor"), lineWidth: 3)
+                                )
+                            Button("Send!", action: {
+                                viewModel.savedText = viewModel.savedText + text
+                            })
+                                .padding()
+                                .frame(width: 140, height: 40, alignment: .center)
+                                .foregroundColor(.white)
+                                .background(Color("primaryColor"))
+                                .clipShape(Capsule()
+                                )
+                            }
+                        }
+                            .padding(.all, 8)
+                    )
+                    .padding(.horizontal, 16)
+            }
+            .navigationTitle("First example")
+
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color("background"))
+    }
+}
+
+struct FirstExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstExampleView()
+    }
+}
